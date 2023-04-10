8e4d73f81790158116acccab50f68c10eda31622
YamaniYassine <yassine145287@gmail.com>
2023-04-10 16:16:39 +0200

    interface modifications

---
+++ frontend/src/pages/RGPD/RGPD.jsx
@@ -0,0 +1,31 @@
+const RGPD = () => {
+    return (
+        <section>
+			<h2>Quelles informations recueillons-nous ?</h2>
+            <ul>
+				<li>Identité : civilité, nom, prénom, adresse, numéro de téléphone, adresse électronique, date de naissance, code interne de traitement permettant l'identification du client, données relatives à l’enregistrement sur des listes d’opposition.</li>
+			</ul>
+            <ul>
+				<li>Données relatives aux jeux : numéro de ticket, type de gain ticket, type de gain compagne.</li>
+			</ul>
+
+
+
+            
+			<p>ThéTipTop est une entreprise spécialisée dans la promotion de gammes de thés de très grande qualité avec des mélanges signatures de l’entreprise, des thés détox, des thés blancs, des thés légumes, infusions, etc. L’ensemble de nos thés sont bios et Handmades.</p>
+			<p>Nous sommes une société créative et passionnée, avec une forte expertise dans l'art du thé.</p>
+            <h2>Notre équipe</h2>
+			<ul>
+				<li>Eric Bourdon - Gérant</li>
+			</ul>
+            <h2>Nos valeurs</h2>
+			<ul>
+				<li>Qualité - Nous nous engageons à offrir des thés de la plus haute qualité, avec des mélanges exclusifs et des ingrédients de première qualité.</li>
+				<li>Créativité - Nous sommes constamment à la recherche de nouvelles idées pour créer des thés uniques et innovants.</li>
+				<li>Responsabilité - Nous sommes soucieux de notre impact environnemental et social, et nous travaillons avec des fournisseurs responsables et éthiques.</li>
+			</ul>
+		</section>
+    );
+};
+
+export default RGPD;
