f127b90f7f2e3554e543422476cb0e868caaec44
YamaniYassine <yassine145287@gmail.com>
2023-04-13 18:38:25 +0200

    user role

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -18,7 +18,6 @@ exports.signup = asyncErrorHandler(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     confirmPassword: req.body.confirmPassword,
-    role: req.body.role,
   });
 
   // const token = generateToken(newUser._id);
