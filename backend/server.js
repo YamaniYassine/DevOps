0a2d3a6c7f2be10939aa29da2d64840db46186b8
YamaniYassine <100916934+YamaniYassine@users.noreply.github.com>
2024-04-27 15:40:20 +0200

    add Prometheus path for metrics in  server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const app = express();
 const morgan = require("morgan");
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
+const promClient = require("prom-client");
+
 dotenv.config({ path: "./config.env" });
 
 // utils
@@ -11,16 +13,27 @@ const globalErrorController = require("./controllers/globalErrorController");
 
 const port = process.env.PORT;
 const dbURI = process.env.DATABASE;
+
+// Prometheus metrics
+const register = new promClient.Registry();
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics({ register });
+
+app.get("/metrics", (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(register.metrics());
+});
+
 // DB connection
- mongoose.connect(dbURI, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true
- });
- const status = mongoose.connection;
- status.on('error', console.error.bind(console, 'error'));
- status.once('open', () => { 
-   console.log('mongoDB connected!');
- });
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const status = mongoose.connection;
+status.on('error', console.error.bind(console, 'error'));
+status.once('open', () => { 
+  console.log('mongoDB connected!');
+});
 
 app.use(express.json());
 if (process.env.NODE_ENV === "developement") {
@@ -34,25 +47,17 @@ const ticketRoutes = require('./routes/ticketRoutes');
 app.use(`/users`, userRoutes);
 app.use('/ticketApi', ticketRoutes);
 
+app.get("/", (req, res) =>{
+  res.status(200).json({message: "hello from server.js"});
+}); 
 
-
-
-
-
- app.get("/", (req, res) =>{
-    res.status(200).json({message: "hello from server.js"});
- }); 
-
-
-
- // handle unhandled routes
+// handle unhandled routes
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find the route ${req.originalUrl}`, 404));
 });
 
 // global error handler
 app.use(globalErrorController);
-
 
 app.listen(port, () => {
   console.log(`App is running at port ${port}`);
