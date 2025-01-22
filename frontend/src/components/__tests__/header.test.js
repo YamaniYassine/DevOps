b3101e1345143dac067791706a33919e7963d36e
YAMANI Yassine <yassine145287@gmail.com>
2025-01-22 20:53:18 +0100

    adding authcontroller test v8

--- frontend/src/components/__tests__/header.test.js
+++ frontend/src/components/__tests__/header.test.js
@@ -2,8 +2,8 @@ import { screen , render , cleanup } from "@testing-library/react";
 import HeaderNav from  "../header/header";
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-jest.mock('../header/header.css', () => ({}));
+// jest.mock('../header/header.css', () => ({}));
 
-test('should render header component', () => {
-    expect(true).toBe(true);
-});
+// test('should render header component', () => {
+//     expect(true).toBe(true);
+// });
