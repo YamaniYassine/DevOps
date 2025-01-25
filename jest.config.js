0d82f5633f9778e2b90d96557177db02d80ebb42
YAMANI Yassine <yassine145287@gmail.com>
2025-01-25 14:41:17 +0100

    fix error 20

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   testEnvironment: "node",
   maxWorkers: 1,
   transformIgnorePatterns: ['<rootDir>/node_modules/(?!(node-fetch|fetch-blob)/)'],
+  moduleNameMapper: {
+    '^formdata-polyfill$': '<rootDir>/node_modules/formdata-polyfill/dist/formdata-polyfill.min.js',
+  },
 };
