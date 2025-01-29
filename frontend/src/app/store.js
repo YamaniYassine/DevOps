2c3c665ea92f8d70e2835e8b8001f620e23fd9fe
YAMANI Yassine <yassine145287@gmail.com>
2025-01-29 14:02:19 +0100

    admin dashboard show/delete users

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import winnerReducer from "../features/auth/winnerSlice";
-import userReducer from './features/auth/userSlice';
+import userReducer from '../features/auth/userSlice';
 
 export const store = configureStore({
   reducer: {
